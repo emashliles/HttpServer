7dd8e08f82cb8dd8569344318b56d69ccfd3eacd
EA <emashliles@gmail.com>
2017-03-01 14:57:50 +0000

    Hardcode first working test.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,20 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print(Basic.test());
+        ServerSocket serverSocket;
+        try {
+            serverSocket = new ServerSocket(5000);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out =
+                    new PrintWriter(clientSocket.getOutputStream(), true);
+            out.println("HTTP/1.1 200 OK");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
